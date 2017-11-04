c63fba0a7efe861e7dc4de73e40942bbb40022d3
Harrison N <tplawyer@gmail.com>
2017-11-04 14:27:21 -0700

    log

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).google;
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
+console.log('BotPopup initiated');
+
 app.post('/', function (request, response) {
   const assistant = new DialogflowApp({request: request, response: response});
   dashbot.configHandler(assistant);
