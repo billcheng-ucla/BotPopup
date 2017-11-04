87fe8b123c3d1d6e8eec1311ea82e8e88e578b53
Harrison N <tplawyer@gmail.com>
2017-11-04 16:30:24 -0700

    server

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-var express = require('express');
-var app = express();
-const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).google;
-const DialogflowApp = require('actions-on-google').DialogflowApp;
+const config = require('./config/config');
+const express = require('express');
+const app = express();
+const basicAuth = require('express-basic-auth');
+const dashbot = require('dashbot')(config.DASHBOT_API_KEY).google;
 
 console.log('BotPopup initiated');
 
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}));
+
 app.get('/', (request, response) => {
-  response.send('hello world');
+  response.send('hello world. but this is not the asdf');
 });
 
 app.post('/', (request, response) => {
-  const assistant = new DialogflowApp({request: request, response: response});
-  dashbot.configHandler(assistant);
+  const sampleResponse = {
+    speech: "Thanks, you order has been received.",
+    displayText: "Thanks, you order has been received.",
+    data: {},
+    contextOut: [],
+    source: ""
+  }
+  response.json(sampleResponse);
 });
 
-app.listen(process.env.HTTP_PORT, () => console.log(`BotPopup listening on port ${process.env.HTTP_PORT}!`))
+app.listen(config.HTTP_PORT, () => console.log(`BotPopup listening on port ${config.HTTP_PORT}!`))
