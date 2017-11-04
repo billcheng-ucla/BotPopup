a8c3b05b2113d869dd034c25c6c50995f9d99c7f
Harrison N <tplawyer@gmail.com>
2017-11-04 14:24:30 -0700

    fix syntax

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const DialogflowApp = require('actions-on-google').DialogflowApp;
 app.post('/', function (request, response) {
   const assistant = new DialogflowApp({request: request, response: response});
   dashbot.configHandler(assistant);
-}
+});
