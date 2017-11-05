c4336801be51d12f638e40e2697aabe1b93d9bef
Harrison N <tplawyer@gmail.com>
2017-11-04 18:06:06 -0700

    config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ module.exports = {
   DASHBOT_API_KEY: 'sNNSiplNYisAa8nlgH5vJ4Cl9vikzdjpAVTbhhQn',
   SLACK_WEBHOOK_HOST: 'hooks.slack.com',
   SLACK_WEBHOOK_ORDERS_CHANNEL: '/services/T7V2LBLM8/B7W66GUET/AaRGeuu1GnfpRQ3ooq7kwhG9',
-  HTTP_PORT: '8080'
+  HTTP_PORT: process.env.HTTP_PORT || '8080'
 };
 
--- slack_helper.js
+++ slack_helper.js
@@ -3,9 +3,15 @@ const request = require('request');
 
 const slackHelper = (messageToSlack) => {
   request.post(
-    `${config.SLACK_WEBHOOK_HOST}${config.SLACK_WEBHOOK_ORDERS_CHANNEL}`,
-    messageToSlack,
-    function (error, response, body) {
+    {
+      method: 'POST',
+      url: `${config.SLACK_WEBHOOK_HOST}${config.SLACK_WEBHOOK_ORDERS_CHANNEL}`,
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(messageToSlack)
+    },
+    (error, response, body) => {
       if (!error && response.statusCode == 200) {
         console.log(body)
       }
