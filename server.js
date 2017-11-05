4cdf19a070ed119be1523604f63cd82dcdb880bd
Harrison N <tplawyer@gmail.com>
2017-11-04 19:00:59 -0700

    order confirmed

--- server.js
+++ server.js
@@ -8,50 +8,56 @@ const slackHelper = require('./slack_helper');
 
 console.log('BotPopup initiated');
 
-app.use(basicAuth({
-  users: { 'admin': 'supersecret' }
-}));
-
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   response.send('hello world. but this is not the asdf');
 });
 
+app.post('/confirm_order', (request, response) => {
+  response.json({
+    'text': 'Order confirmed'
+  });
+});
+
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}));
+
 app.post('/', (request, response) => {
   slackHelper({
     "text": "A customer just ordered a coffee. Please confirm.",
     "attachments": [
-        {
-            "text": "Confirm Order?",
-            "fallback": "Failed to confirm order",
-            "callback_id": "order_confirm_id",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "order",
-                    "text": "Confirm",
-                    "type": "button",
-                    "value": "confirm"
-                },
-                {
-                    "name": "order",
-                    "text": "Cancel",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "cancel",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "The customer will be very dissapointed... :(",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
-                    }
-                }
-            ]
-        }
+      {
+        "text": "Confirm Order?",
+        "fallback": "Failed to confirm order",
+        "callback_id": "order_confirm_id",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "actions": [
+          {
+            "name": "order",
+            "text": "Confirm",
+            "type": "button",
+            "value": "confirm"
+          },
+          {
+            "name": "order",
+            "text": "Cancel",
+            "style": "danger",
+            "type": "button",
+            "value": "cancel",
+            "confirm": {
+              "title": "Are you sure?",
+              "text": "The customer will be very dissapointed... :(",
+              "ok_text": "Yes",
+              "dismiss_text": "No"
+            }
+          }
+        ]
+      }
     ]
-});
+  });
   dashbot.logIncoming(request.body);
   const sampleResponse = {
     speech: "Thanks, you order has been received.",
@@ -62,10 +68,6 @@ app.post('/', (request, response) => {
   }
   dashbot.logOutgoing(request.body, sampleResponse);
   response.json(sampleResponse);
-});
-
-app.post('/confirm_order', (request, response) => {
-  console.log(request);
 });
 
 app.listen(config.HTTP_PORT, () => console.log(`BotPopup listening on port ${config.HTTP_PORT}!`))
